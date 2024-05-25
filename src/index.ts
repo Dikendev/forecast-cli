970d32a80a823d6890d8a6987d904d8e48d8c6fd
Dikendev <diken.dev@gmail.com>
2024-05-24 22:17:01 -0300

    feat add initial interfaces / initial options

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,52 @@
 #!/usr/bin/env node
 import minimist from "minimist";
+import { Help } from "./help";
+
+export const Options = {
+	help: "help",
+	version: "version",
+} as const;
+
+export type Options = typeof Options;
+
+export interface HelpOption {
+	help: string;
+	h: string;
+}
+
+export interface VersionOption {
+	version: string;
+	v: string;
+}
+
+export interface CommandLineOptions extends HelpOption, VersionOption {}
+
+export type Commands = keyof typeof Options;
 
 (() => {
 	console.log("Hello CLI");
 	const args = minimist(process.argv.slice(2));
-	console.log(args);
+	let cmd = (args._[0] as Commands) || Options.help;
+
+	const cast = args as unknown as CommandLineOptions;
+
+	if (cast.version || cast.v) {
+		cmd = Options.version;
+	}
+
+	if (cast.help || cast.h) {
+		cmd = Options.help;
+	}
+
+	switch (cmd) {
+		case Options.help:
+			Help.showHelp();
+			break;
+		case Options.version:
+			console.log("Version executed");
+			break;
+		default:
+			console.error(`"${cmd}" is not a valid command!`);
+			break;
+	}
 })();
