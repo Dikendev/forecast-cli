3d3b5d1784a9842c1e1bb3dbf1e2132fe0319e6b
Dikendev <diken.dev@gmail.com>
2024-05-25 23:30:46 -0300

    feat add help

--- src/help.ts
+++ src/help.ts
@@ -1,5 +1,35 @@
+import minimist from "minimist";
+
+export interface MenuOptions {
+	[key: string]: string;
+}
+
+const menus: MenuOptions = {
+	main: `
+			weather [command] <options>
+			today .............. show weather for today
+			forecast ........... show 10-day weather forecast
+			version ............ show package version
+			help ............... show help menu for a command`,
+	today: `
+			weather today <options>
+			--location, -l ..... the location to use`,
+};
+
 export class Help {
-	static showHelp() {
-		console.log("Help is on the way!");
+	static menu(args: minimist.ParsedArgs) {
+		const valid = args._.find((arg) => arg.includes("today"));
+
+		switch (valid) {
+			case "main":
+				console.log(menus.main);
+				break;
+			case "today":
+				console.log(menus[valid]);
+				break;
+			default:
+				console.log(menus.main);
+				break;
+		}
 	}
 }
