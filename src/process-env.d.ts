cd4834575801a8ac42686f25a3d5f72a56a645ca
Dikendev <diken.dev@gmail.com>
2024-06-08 23:57:12 -0300

    add env types

--- src/process-env.d.ts
+++ src/process-env.d.ts
@@ -1,12 +1,12 @@
-// declare global {
-// 	namespace NodeJS {
-// 		interface ProcessEnv {
-// 			[key: string]: string | undefined;
-// 			WEATHER_API: string;
-// 			OPEN_WEATHER_TOKEN: string;
-// 			// add more environment variables and their types here
-// 		}
-// 	}
-// }
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			[key: string]: string | undefined;
+			WEATHER_API: string;
+			OPEN_WEATHER_TOKEN: string;
+			OPEN_WEATHER_API: string;
+		}
+	}
+}
 
-// export {};
+export {};
