4b2712c1b32674c6c1083d17ead4d9b467b860fc
Dikendev <diken.dev@gmail.com>
2024-06-09 23:48:57 -0300

    add state main option

--- src/help.ts
+++ src/help.ts
@@ -9,6 +9,7 @@ const menus: MenuOptions = {
 			weather [command] <options>
 			today .............. show weather for today
 			forecast ........... show 10-day weather forecast
+			states ............. show list of states
 			version ............ show package version
 			help ............... show help menu for a command`,
 	today: `
