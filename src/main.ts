6037da4fc7746d0e3d744bbe36ee7fd896d42c24
Dikendev <diken.dev@gmail.com>
2024-06-09 23:50:50 -0300

    add option / format to console

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,33 @@ import { Today } from "./cmd/today";
 import { version } from "./version";
 import { Options } from "./interfaces/options.interface";
 import { Commands, CommandLineOptions } from "./interfaces/command.interface";
+import { States } from "./cmd/states";
+import Table from "cli-table3";
+import { FilteredWeatherData } from "./service/interfaces/open-weather.interface";
 
-(() => {
+export interface TableData {
+	[key: string]: string;
+}
+
+function convertToArray(response: FilteredWeatherData): TableData[] {
+	const data = Object.entries(response).map(([key, value]) => ({
+		[key]: value,
+	}));
+	return data;
+}
+
+const printResponse = (response: FilteredWeatherData) => {
+	var table = new Table();
+
+	const data = convertToArray(response);
+
+	data.forEach((item) => {
+		table.push(item);
+	});
+	console.log(table.toString());
+};
+
+(async () => {
 	const args = minimist(process.argv.slice(2));
 	let cmd = (args._[0] as Commands) || Options.help;
 
@@ -23,15 +48,24 @@ import { Commands, CommandLineOptions } from "./interfaces/command.interface";
 	switch (cmd) {
 		case Options.today:
 			const today = new Today();
-			today.getToday();
-			console.log("Today is sunny!");
+			const response = await today.getToday(args);
+
+			if (!response) {
+				console.error("Something went wrong");
+				return;
+			}
+
+			printResponse(response);
 			break;
 		case Options.help:
-			console.log("Help menu", cast);
 			Help.menu(args);
 			break;
 		case Options.version:
 			version();
+			break;
+		case Options.state:
+			const states = new States();
+			states.getStates();
 			break;
 		default:
 			console.error(`"${cmd}" is not a valid command!`);
