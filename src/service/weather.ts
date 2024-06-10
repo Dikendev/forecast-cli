6282c0a63bbad4b325170cd6ddec32ec704bef30
Dikendev <diken.dev@gmail.com>
2024-06-09 23:49:59 -0300

    update response interface result / interface name fix

--- src/service/weather.ts
+++ src/service/weather.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { WeatherResponse } from "./interfaces/weather.interface";
-import { METRICS, MetricsType } from "../constants/metrics.constant";
+import { METRICS, Metrics } from "../constants/metrics.constant";
 import dotenv from "dotenv";
+import { OpenWeatherApi } from "./interfaces/open-weather.interface";
 dotenv.config();
 
 export class WeatherParams {
 	constructor(
-		public readonly lat: number,
-		public readonly lon: number,
-		public readonly units: MetricsType
+		public readonly lat: string,
+		public readonly lon: string,
+		public readonly units: Metrics
 	) {}
 }
 
@@ -48,20 +49,16 @@ export class Weather {
 	}
 
 	async getWeather(
-		lat: number,
-		long: number,
-		metrics: MetricsType
-	): Promise<WeatherResponse> {
-		// const url = `https://api.tomorrow.io/v4/weather/forecast?location=${coordinates}&apikey=WezdiiKDlXT2ceg1POgROArCJfJKgT0F`;
-
+		lat: string,
+		long: string,
+		metrics: Metrics
+	): Promise<OpenWeatherApi> {
 		const coordinates = new WeatherParams(lat, long, metrics);
-
 		const params = getParams(coordinates);
-
 		const url = `${this.openweatherMapApi}/data/2.5/weather?${params}&appid=${this.token}`;
 
 		try {
-			const result = await axios.get<WeatherResponse>(url);
+			const result = await axios.get<OpenWeatherApi>(url);
 
 			if (!result.data) {
 				throw new Error("No data returned from the weather API");
